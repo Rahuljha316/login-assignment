8246c8e43685f5a55261ee9ad8d6f37ac6d154d2
Rahul Jha <rahulbudy11@gmail.com>
2023-03-21 17:57:51 +0530

    initiated the project

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import Logo from '../assets/reactLogo.png'
+
+const Main = () =>{
+    return (
+        <div>
+            <img alt="Logo" src={Logo}/>
+            <div>SOME RANDOM TEXT, SOME RANDOM TEXT,SOME RANDOM TEXT,SOME RANDOM TEXT</div>
+        </div>
+    )
+}
+
+export default Main;
